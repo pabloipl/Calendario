ebf9d3beeb9508a7cc5e875cf28abf4c61910d37
pabloipl <p.perez14@ufromail.cl>
2022-09-27 09:58:41 -0300

    Commit Pablo

---
+++ src/main/java/TodaysDate.java
@@ -0,0 +1,16 @@
+import java.util.GregorianCalendar;
+
+public class TodaysDate {
+    String time;
+    public int day;
+    private int month;
+    protected int year;
+
+    public void printDateAndTime(){
+        GregorianCalendar calendar = new GregorianCalendar();
+        time =  calendar.get(Calendar.HOUR_OF_DAY) + ":"
+                + calendar.get(Calendar.MINUTE) + ":"
+                + calendar.get(Calendar.SECOND);
+
+    }
+}
